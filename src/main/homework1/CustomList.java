9738f634aedfeacef91074723b18fdfc54edcef0
ariel gonzales <aaggon411@gmail.com>
2019-05-15 23:13:23 -0400

    added double linkedlist and improve tests

--- src/main/homework1/CustomList.java
+++ src/main/homework1/CustomList.java
@@ -1,9 +1,20 @@
 package homework1;
 
+
+/**
+ * CustomList class.
+ */
 public class CustomList implements IList {
     Node head;
     Node tail;
 
+    /**
+     * Function that involve add node
+     * by index into the list.
+     *
+     * @param index int  index.
+     * @param value T    value.
+     */
     @Override
     public void add(int index, Object value) {
         Node newNode = new Node(value);
@@ -16,10 +27,18 @@ public class CustomList implements IList {
                 currentNode = currentNode.next;
             }
             newNode.next = currentNode.next;
+            currentNode.next.previous = newNode;
+            newNode.previous = currentNode;
             currentNode.next = newNode;
         }
     }
 
+    /**
+     * Function that involve add node
+     * at the head into the list.
+     *
+     * @param value T    value.
+     */
     @Override
     public void addFirst(Object value) {
         Node newNode = new Node(value);
@@ -27,11 +46,18 @@ public class CustomList implements IList {
             head = newNode;
             tail = newNode;
         } else {
+            head.previous = newNode;
             newNode.next = head;
             head = newNode;
         }
     }
 
+    /**
+     * Function that involve add node
+     * at the tail into the list.
+     *
+     * @param value T    value.
+     */
     @Override
     public void addLast(Object value) {
         Node newNode = new Node(value);
@@ -39,34 +65,54 @@ public class CustomList implements IList {
             tail = newNode;
         } else {
             tail.next = newNode;
+            newNode.previous = tail;
             tail = newNode;
         }
     }
 
+    /**
+     * Function that involve remove node
+     * by index into the list.
+     *
+     * @param index int  index.
+     */
     @Override
     public void remove(int index) {
         Node currentNode = head;
         for (int i = 1; i < index; i++) {
             currentNode = currentNode.next;
         }
+
         currentNode.next = currentNode.next.next;
+        currentNode.next.next.previous = currentNode;
     }
 
+
+    /**
+     * Function that involve remove head node
+     * on the list.
+     */
     @Override
     public void removeFirst() {
         head = head.next;
+        head.previous = null;
     }
 
+
+    /**
+     * Function that involve remove tail node
+     * on the list.
+     */
     @Override
     public void removeLast() {
-        Node currentNode = head;
-        while (currentNode.next.next != null) {
-            currentNode = currentNode.next;
-        }
-        tail = currentNode;
-        currentNode.next = null;
+        tail = tail.previous;
+        tail.next = null;
     }
 
+    /**
+     * Function that show the list
+     * elements.
+     */
     @Override
     public void printList() {
         Node currentNode = head;
@@ -79,6 +125,13 @@ public class CustomList implements IList {
         }
     }
 
+    /**
+     * Function that return specific node
+     * into the list.
+     *
+     * @param index int  index.
+     * @return node value
+     */
     @Override
     public Node getIndex(int index) {
         Node currentNode = head;
--- src/test/homework1/CustomListTest.java
+++ src/test/homework1/CustomListTest.java
@@ -8,12 +8,18 @@ public class CustomListTest {
 
     private CustomList customList;
 
+    /**
+     * Init list object.
+     */
     @org.junit.Before
     public void init() {
         customList = new CustomList();
     }
 
 
+    /**
+     * Test that involve add method.
+     */
     @org.junit.Test
     public void add() {
 
@@ -24,27 +30,62 @@ public class CustomListTest {
         customList.add(2, 8);
         customList.add(3, 9);
         customList.add(4, 10);
+        customList.add(5, "OBJECT");
+        customList.add(6, true);
+        customList.add(7, 17.3);
         customList.addLast(4);
 
         assertEquals(customList.getIndex(2).value, 8);
         assertEquals(customList.getIndex(4).value, 10);
-        assertEquals(customList.getIndex(5).value, 2);
+        assertEquals(customList.getIndex(5).value, "OBJECT");
+        assertEquals(customList.getIndex(6).value, true);
+        assertEquals(customList.getIndex(7).value, 17.3);
 
     }
 
-
+    /**
+     * Test that involve addFirst method.
+     */
     @org.junit.Test
     public void addFirst() {
         customList.addFirst(1);
+
         assertEquals(customList.head.value, 1);
     }
 
+    /**
+     * Test that involve addFirstString method.
+     */
+    @org.junit.Test
+    public void addFirstString() {
+        customList.addFirst("First");
+
+        assertEquals(customList.head.value, "First");
+    }
+
+    /**
+     * Test that involve addLast method.
+     */
     @org.junit.Test
     public void addLast() {
         customList.addLast(85);
+
         assertEquals(customList.tail.value, 85);
     }
 
+    /**
+     * Test that involve addLastString method.
+     */
+    @org.junit.Test
+    public void addLastString() {
+        customList.addLast("TAIL");
+
+        assertEquals(customList.tail.value, "TAIL");
+    }
+
+    /**
+     * Test that involve remove method.
+     */
     @org.junit.Test
     public void remove() {
         customList.addFirst(1);
@@ -53,29 +94,42 @@ public class CustomListTest {
         customList.addLast(4);
 
         customList.remove(1);
+
         assertNotEquals(customList.getIndex(1), 4);
     }
 
+    /**
+     * Test that involve removeFirst method.
+     */
     @org.junit.Test
     public void removeFirst() {
         customList.addFirst(1);
-        customList.add(1, 2);
-        customList.add(2, 3);
-        customList.addLast(4);
+        customList.addFirst(2);
+        customList.add(1, 3);
+        customList.add(2, 4);
+        customList.addLast(5);
 
         customList.removeFirst();
+
         assertNotEquals(customList.getIndex(0), 1);
     }
 
+    /**
+     * Test that involve removeLast method.
+     */
     @org.junit.Test
     public void removeLast() {
         customList.addFirst(1);
         customList.addLast(4);
 
         customList.removeLast();
+
         assertNotEquals(customList.getIndex(1), 4);
     }
 
+    /**
+     * Test that involve getIndex method.
+     */
     @org.junit.Test
     public void getIndex() {
 
@@ -84,7 +138,6 @@ public class CustomListTest {
         customList.add(1, 9);
         customList.add(2, 10);
         customList.addLast(4);
-
 
         assertEquals(customList.getIndex(0).value, 8);
         assertEquals(customList.getIndex(1).value, 9);
